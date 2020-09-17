a55e2da5227835a298f169ae853242ff84ddf716
Arna Ásgeirsdóttir <arna@Arnas-MBP-4.hringdu>
2020-09-17 10:39:07 +0000

    "third"

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,6 +1,16 @@
-#halló
+#Við höfum hefur leikborð sem er 3x3  
+#Forrit tilkynnir leikmanni hvert hann getur ferðast miðað við hvar hann er
+    #Gefið er að leikmaður byrjar í 1,1 á leikborði
+#Forrit tekur við input átt, ber saman við staðsetningu, og tilkynnir hvert ferðast
+    #ef röng átt er valin eru skilaboð um villu send tilbaka
 
 
-x = 3
+def direction():
 
-print(x)
+def can_travel():
+
+
+for i in range(1,4):
+    for j in range(1,4):
+        #byrjar í i, j (1,1)
+        #forrit tilkynnir hvert getur farið
